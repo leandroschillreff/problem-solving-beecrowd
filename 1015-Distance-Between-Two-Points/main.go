2c7b5570e197f8b75ac0bf3629a8091d48296800
Leandro Schillreff <leandroschillreff@gmail.com>
2026-05-09 14:09:09 -0400

    feat: add solution for '1015 Distance Between Two Points'

---
+++ 1015-Distance-Between-Two-Points/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	var x1, x2, y1, y2 float64
+
+	fmt.Scan(&x1, &y1, &x2, &y2)
+
+	distance := math.Sqrt(math.Pow((x2-x1), 2) + math.Pow((y2-y1), 2))
+	fmt.Printf("%.4f\n", distance)
+}
